c4b86196046368824a6895c1e97c928ba0f47542
TobiasHeide <slepzs@gmail.com>
2023-07-19 20:45:56 +0200

    Fix: removed prettier and updated confg

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '**/*.ts?(x)': (filenames) =>
-    `next lint --fix --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(' --file ')}`,
-  '**/*.{css,scss}': () => `npx stylelint **/*.{css,scss}`,
-}
--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa");
-
-const nextConfig = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
 });
+
+const nextConfig = withPWA({});
 
 module.exports = nextConfig;
